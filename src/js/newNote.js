248366179b0552115609c241a7d46aed8b5272c1
restlesswizard <erxme@yandex.ru>
2023-05-27 16:08:25 +0500

    feat: add new note by submitting button

--- src/js/newNote.js
+++ src/js/newNote.js
@@ -1,10 +1,12 @@
 let btnNewNote = document.querySelector('.add_note')
+let sectionNotes = document.querySelector('.notes')
 
-const noteCreate = 
-'<div class="note-create">' +
-  '<input type="text" class="note-create__text" placeholder="Write some text here...">' +
-  '<button class="note-create__submit-btn">V</button>' +
-'</div>'
+const noteCreate = `
+<div class="note-create">
+  <input type="text" class="note-create__text" placeholder="Write some text here...">
+  <button class="note-create__submit-btn">V</button>
+</div>
+`
 
 btnNewNote.addEventListener('click', () => {
 	console.log('clicked');
@@ -15,14 +17,38 @@ btnNewNote.addEventListener('click', () => {
 })
 
 function submitNewNote() {
-	let note = document.querySelector('.note-create')
-	note.addEventListener('click', () => {
-		console.log('submit')
+	let noteCreateContainer = document.querySelector('.note-create')
+	let noteSubmit = document.querySelector('.note-create__submit-btn')
+	let noteText = document.querySelector('.note-create__text')
+
+	noteSubmit.addEventListener('click', function listener() {
+
+		if (noteText.value === '') {
+			//Empty string check
+			console.log('Error! Enter some text')
+		} else {
+			// console.log('submit')
+			noteSubmit.removeEventListener('click', listener)
+			noteCreateContainer.remove()
+			btnNewNote.disabled = false
+
+			//This value to localstorage
+			createNewNote(noteText.value)
+		}
 	})
 }
 
-console.log(noteCreate)
-
+function createNewNote(text) {
+	sectionNotes.insertAdjacentHTML('afterbegin', `
+	<div class="note">
+		<div class="note__check">
+			<input type="checkbox" name="note-checkbox1" id="note-checkbox1">
+		</div>
+		<p class="note__text" >${text}</p>
+			<button class="note__delete-btn">X</button>
+	</div>
+	`)
+}
 
 
 
